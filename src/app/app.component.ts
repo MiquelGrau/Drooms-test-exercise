454be9354df6cd71cce4181e91620d57a095799c
MiquelGrau <mikigrca@gmail.com>
2023-09-12 18:37:50 +0200

    feat: some simple style added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, OnInit } from '@angular/core';
+import { ParticlesConfig } from '../assets/particlesjs-config';
+
+declare let particlesJS: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Drooms-test-exercise';
+
+  constructor(private el: ElementRef) {}
+
+  public ngOnInit(): void {
+    this.invokeParticles();
+  }
+
+  public invokeParticles(): void {
+    particlesJS('particles-js', ParticlesConfig, function() {});
+  }
 }
